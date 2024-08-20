dba164c4641123a7d6fe9d0b1d50a5e6664a90e5
hamzabawumia <hamzabawumia@yahoo.com>
2024-08-20 13:38:17 -0700

    fixing the commit

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+module.exports = function(eleventyConfig) {
+  return {
+    dir: {
+      input: "study_effectively", // This is your source directory
+      output: "study_effectively/_site" // This is where Eleventy will build the site
+    }
+  };
+};
